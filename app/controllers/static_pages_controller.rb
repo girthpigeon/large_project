fb845bad77d3d82facab12071339d18fcda0f992
Zack <pajka@wisc.edu>
2014-04-14 17:27:26 -0500

    fixed some css styling and made it look more pretty

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,11 @@ class StaticPagesController < ApplicationController
   def home
       if signed_in?
        @dare = current_user.dares.build
-       @feed_items = current_user.feed.paginate(page: params[:page])
-     else
+       #@feed_items = current_user.feed.paginate(page: params[:page])
+       #else
+       end
      @feed_items = Dare.paginate(:page => params[:page], :per_page => 30)
-     end
+     #end
   end
 
   def help
