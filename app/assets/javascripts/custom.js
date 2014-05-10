55fbc2b735c49cce2663707683cc55a2b7e1c375
Zack <pajka@wisc.edu>
2014-05-10 14:54:34 -0500

    Fixed submitting empty dare bug

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,4 +2,13 @@ $(document).ready(function () {
 	$('.acceptButton').click(function () {
 		$(this).val("Accepted");
 	    });
+
+        $('.btn').attr('disabled',true);
+    $('#dareContent').keyup(function(){
+        if($(this).val().length !=0){
+            $('.sendButton').attr('disabled', false);
+        }
+        else
+        $('.sendButton').attr('disabled',true);
+    })
     });
